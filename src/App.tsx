897f86e7acd73da8e11bc8e9cdaad006dee4d5b4
loggs <lcsugit@gmail.com>
2025-03-26 18:35:27 -0400

    upgrade typescript, get BlockNote working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+ 
+export default function App() {
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote();
+ 
+  // Renders the editor instance using a React component.
+  return <BlockNoteView editor={editor} />;
 }
-
-export default App;
